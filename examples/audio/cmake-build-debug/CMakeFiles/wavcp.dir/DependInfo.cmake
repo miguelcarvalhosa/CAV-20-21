9d0be2be9e41d548d37c1dad50bcc55a5576ac95
Tânia <tania.s.ferreira@ua.pt>
2020-10-25 18:31:16 +0000

    "comecei o projeto do deliverable 1 - ficheiros restantes"

--- examples/audio/cmake-build-debug/CMakeFiles/wavcp.dir/DependInfo.cmake
+++ examples/audio/cmake-build-debug/CMakeFiles/wavcp.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/miguel/Desktop/CAV-20-21/examples/audio/wavcp.cpp" "/home/miguel/Desktop/CAV-20-21/examples/audio/cmake-build-debug/CMakeFiles/wavcp.dir/wavcp.cpp.o"
+  "/home/tania/Desktop/CAV-20-21/examples/audio/wavcp.cpp" "/home/tania/Desktop/CAV-20-21/examples/audio/cmake-build-debug/CMakeFiles/wavcp.dir/wavcp.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
