c5acf099acdd8e884c64b0f0c99f5cd0409a4e5d
Tânia <tania.s.ferreira@ua.pt>
2020-11-02 00:24:34 +0000

    bugs nos pontos 4 e 5 corrigidos

--- deliverable 1/point4.cpp
+++ deliverable 1/point4.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <sndfile.hh>
+#include <cmath>
+#include <fstream>
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 
@@ -11,24 +13,31 @@ constexpr size_t FRAMES_BUFFER_SIZE = 65536; // Buffer for reading/writing frame
 
 int main(int argc, char *argv[]) {
     /* set number of bins and range of values*/
-    int histSize = 256;
-    float range[] = { 0, pow(2,16) };
+    int histSize = pow(2,16);
+    float range[] = { 0, 2*32767 };
     const float* histRange = { range };
     /* set the bins to have the same size (uniform) and to clear previous existent histograms (accumulate) */
     bool uniform = true, accumulate = false;
     /* output histogram variables */
-    Mat r_hist, l_hist;
+    Mat r_hist, l_hist, mono_hist;
+    Mat samples_mono, samples_r, samples_l;
     int nImages = 1;
     /* list of dims channels */
     int channels = 0; // there is only one channel, the intensity
 
+    size_t nFrames;
+    short buf [FRAMES_BUFFER_SIZE*2];
+    short buf_r [FRAMES_BUFFER_SIZE], buf_l[FRAMES_BUFFER_SIZE], buf_mono[FRAMES_BUFFER_SIZE];
 
-    if(argc < 3) {
+    /* file for storing calculated frequencies */
+    ofstream outfile(argv[2]);
+    if(argc < 2) {
         cerr << "Usage: wavcp <input file> <output file>" << endl;
         return 1;
     }
 
     SndfileHandle sndFileIn { argv[argc-2] };
+
     if(sndFileIn.error()) {
         cerr << "Error: invalid input file" << endl;
         return 1;
@@ -49,49 +58,32 @@ int main(int argc, char *argv[]) {
     cout << '\t' << sndFileIn.samplerate() << " samples per second" << endl;
     cout << '\t' << sndFileIn.channels() << " channels" << endl;
 
-    SndfileHandle sndFileOut { argv[argc-1], SFM_WRITE, sndFileIn.format(),
-                               sndFileIn.channels(), sndFileIn.samplerate() };
-    if(sndFileOut.error()) {
-        cerr << "Error: invalid output file" << endl;
-        return 1;
-    }
 
-    size_t nFrames;
     vector<short> samples(FRAMES_BUFFER_SIZE * sndFileIn.channels());
-    // reads nFrames from source audio file to destination file
-    while((nFrames = sndFileIn.readf(samples.data(), FRAMES_BUFFER_SIZE)))
-        sndFileOut.writef(samples.data(), nFrames);
+    nFrames = sndFileIn.readf(samples.data(), FRAMES_BUFFER_SIZE);
 
-    Mat samples_r (1,FRAMES_BUFFER_SIZE ,CV_16UC1, samples[0]);
-    Mat samples_l (1,FRAMES_BUFFER_SIZE ,CV_16UC1, samples[1]);
-    /* calculate histogram for each plane: BGR */
+    /* calHist() function does not calculate histogram for signed data. Therefore
+     * the range of values between -32767 and 32767 is converted to 0 to 2*32767
+     * in order to compute the frequency of each value */
+    for(int i=0; i<FRAMES_BUFFER_SIZE*sndFileIn.channels(); i=i+2) {
+        buf_r[i] = samples[i]+32767;
+        buf_l[i]= samples[i+1]+32767;
+        buf_mono[i] = cvRound((buf_r[i] + buf_l[i])/2)+32767;
+        cout << i << " " << buf[i] << " " << buf[i+1] << endl;
+    }
+    /* convert array to Mat in order to calculate the histograms */
+    samples_mono= Mat(1,FRAMES_BUFFER_SIZE,CV_16U, buf_mono);
+    samples_r = Mat(1,FRAMES_BUFFER_SIZE,CV_16U, buf_r);
+    samples_l= Mat(1,FRAMES_BUFFER_SIZE,CV_16U, buf_l);
+
+
+    /* calculate histogram for each channel */
     calcHist( &samples_r, nImages, &channels, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
     calcHist( &samples_l, nImages, &channels, Mat(), l_hist, 1, &histSize, &histRange, uniform, accumulate );
-
-    /* generate image to display */
-    int hist_w = 512, hist_h = 400;
-    int bin_w = cvRound( (double) hist_w/histSize );
-    Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
-
-    /* normalize each histogram between 0 and histImage.rows in order to display it */
-    /*    @ dtype = -1: src and dst are the same type (Mat)           */
-    /*    @ norm_type = adjust values between the specified range:    */
-    normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-    normalize(l_hist, l_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-
-    for( int i = 0; i < histSize+1; i++ )
-    {
-        line( histImage, Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-              Point( bin_w*(i+1), hist_h - cvRound(r_hist.at<float>(i+1)) ),
-              Scalar( 255, 0, 0), 2, 8, 0  );
-
-        line( histImage, Point( bin_w*(i), hist_h - cvRound(l_hist.at<float>(i)) ),
-              Point( bin_w*(i+1), hist_h - cvRound(l_hist.at<float>(i+1)) ),
-              Scalar( 0, 255, 0), 2, 8, 0  );
+    calcHist( &samples_mono, nImages, &channels, Mat(), mono_hist, 1, &histSize, &histRange, uniform, accumulate );
+    for (int k=0; k<FRAMES_BUFFER_SIZE; k++) {
+        outfile << r_hist.at<short>(k) << " " << l_hist.at<short>(k) << " " << mono_hist.at<short>(k) << endl;
     }
-    imshow("BGR Histogram", histImage );
-    waitKey(0);
+
     return 0;
-
 }
-
--- deliverable 1/point5.cpp
+++ deliverable 1/point5.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 {
     string header; // store the header
     int yCols, yRows; /* frame dimension */
-    int end = 0;
+    int end = 0, pause = 0;
     int y, u, v, r, g, b;
 
     /* set number of bins and range of values*/
@@ -45,12 +45,12 @@ int main(int argc, char *argv[])
     int nImages = 1;
     /* list of dims channels */
     int channels = 0; // there is only one channel, the intensity
-
     float b_ent, g_ent, r_ent, gray_ent;
     char inputKey = '?'; /* parse the pressed key */
 
     /* Opening input and output video file */
     ifstream infile (argv[1]);
+    ofstream outfile(argv[2]);
 
     /* Processing header */
     getline (infile,header);
@@ -71,19 +71,21 @@ int main(int argc, char *argv[])
     uchar *buffer;
     int count=0;
 
-    cout << "-------------------------- ENTROPY -----------------------" << endl;
-    while(!end) {
 
+    cout << "-------------------------- ENTROPY -----------------------" << endl;
+
+    vector<uchar> rr, gg, bb, gray;
+    while(!end) {
+        count++;
         getline (infile,header); // Skipping word FRAME
         /* load one frame into buffer frameData */
         infile.read((char *)frameData, yCols * yRows * 3);
-        //uchar (*b)[yCols] = reinterpret_cast<uchar (*)[yCols]>(frameData);
         /* stop process when last frame is detected */
         if(infile.eof())
         {
             cout << "-----------------------------------------------------------" << endl;
-            waitKey();
             infile.close();
+            outfile.close();
             end = 1;
             break;
         }
@@ -128,59 +130,18 @@ int main(int argc, char *argv[])
         /* calculate histogram for the gray scale version */
         calcHist( &gray_img, nImages, &channels, Mat(), gray_hist, 1, &histSize, &histRange, uniform, accumulate );
 
-        merge(bgr_planes, color_img );
-        /* generate image to display */
-        int hist_w = 512, hist_h = 400;
-        int bin_w = cvRound( (double) hist_w/histSize );
-        Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
-
-        /* normalize each histogram between 0 and histImage.rows in order to display it */
-        /*    @ dtype = -1: src and dst are the same type (Mat)           */
-        /*    @ norm_type = adjust values between the specified range:    */
-        normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-        normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-        normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-        normalize(gray_hist, gray_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-
-        for( int i = 0; i < histSize+1; i++ )
-        {
-            line( histImage, Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
-                  Point( bin_w*(i+1), hist_h - cvRound(b_hist.at<float>(i+1)) ),
-                  Scalar( 255, 0, 0), 2, 8, 0  );
-
-            line( histImage, Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-                  Point( bin_w*(i+1), hist_h - cvRound(g_hist.at<float>(i+1)) ),
-                  Scalar( 0, 255, 0), 2, 8, 0  );
-
-            line( histImage, Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-                  Point( bin_w*(i+1), hist_h - cvRound(r_hist.at<float>(i+1)) ),
-                  Scalar( 0, 0, 255), 2, 8, 0  );
-
-            line( histImage, Point( bin_w*(i), hist_h - cvRound(gray_hist.at<float>(i)) ),
-                  Point( bin_w*(i+1), hist_h - cvRound(gray_hist.at<float>(i+1)) ),
-                  Scalar( 255, 255, 255), 2, 8, 0  );
-            line( histImage, Point( bin_w*(i), hist_h - cvRound(gray_hist.at<float>(i)) ),
-                  Point( bin_w*(i+1), hist_h - cvRound(gray_hist.at<float>(i+1)) ),
-                  Scalar( 255, 255, 255), 2, 8, 0  );
+        for (int k=0; k<histSize; k++) {
+            outfile << count << " " << b_hist.at<float>(k) << " " << g_hist.at<float>(k) << " " << r_hist.at<float>(k) << " " << gray_hist.at<float>(k) << endl;
         }
-        imshow("BGR Histogram", histImage );
-        //imshow("BGR Histogram", color_img);
-        //imshow("BGR Histogram", gray_img);
-        inputKey = waitKey(1.0 / 50 * 1000);
-        if ((char)inputKey == 'q') {
-            end = 1;
-            break;
-        }
-
+        /* calculate entropy */
         b_ent = entropy(b_hist);
         g_ent = entropy(g_hist);
         r_ent = entropy(r_hist);
         gray_ent = entropy(gray_hist);
 
-        count++;
         cout << "[" << count << "]";
         cout << "(B,G,R) " << "(" << b_ent << ", " << g_ent << ", " << r_ent << ") " << "& (GRAY) " << gray_ent << endl;
     }
+
     return EXIT_SUCCESS;
 }
-
