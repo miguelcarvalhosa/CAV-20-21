230aeab21ea357505b6c3cc6c3f1d8d17bca5e7b
Goncalo Cardoso <gnvcardoso@ua.pt>
2020-11-27 15:56:53 +0000

    Corrigido problema da bitstream write

--- deliverable 3/BitStream_Write.cpp
+++ deliverable 3/BitStream_Write.cpp
@@ -2,8 +2,7 @@
 
 
 BitStream_Write::BitStream_Write() {
-    buffer = 0x0;       // Initialize the buffer empty
-    pos = 7;            // Point to the MSB of the buffer as the first free position
+
 }
 
 BitStream_Write::BitStream_Write(std::string fileName) {
@@ -11,22 +10,28 @@ BitStream_Write::BitStream_Write(std::string fileName) {
     if(!fp) {
         std::cerr << "ERROR in BitStream_Write: Could not open file \"" << fileName << "\"." << std::endl;
     }
+    buffer = 0x0;       // Initialize the buffer empty
+    pos = 7;            // Point to the MSB of the buffer as the first free position
 }
-
 
 BitStream_Write::~BitStream_Write() {
     close();
 }
 
-void BitStream_Write::setFileName(std::string fileName) {
-    
-}
 
 void BitStream_Write::close() {
     if(pos != -1) {             // If the buffer is not full, to not loose the information when the bitstream is closed
         writeNBits(0x00, pos+1);    // fill the remaining positions with zeros and write the entire buffer
     }
     fp.close();     // Close the file
+}
+void BitStream_Write::setFileName(std::string fileName) {
+    fp.open(fileName, std::fstream::out | std::fstream::binary);        // Open the file
+    if(!fp) {
+        std::cerr << "ERROR in BitStream_Write: Could not open file \"" << fileName << "\"." << std::endl;
+    }
+    buffer = 0x0;       // Initialize the buffer empty
+    pos = 7;            // Point to the MSB of the buffer as the first free position
 }
 
 
@@ -55,7 +60,7 @@ void BitStream_Write::writeBit(uint8_t val) {
 void BitStream_Write::writeNBits(uint64_t val, uint8_t nbits) {
     if(nbits >= 1 && nbits <= 64) {                 // Validate the argument 'nbits'
         for (int i=nbits; i>=1; i--) {              // Iterate through each of the bits in the word 'val', from
-                                                    // 'nbits' to the LSB
+            // 'nbits' to the LSB
             uint8_t bit = (val >> (i-1)) & 0x1;     // Isolate each bit, using right shifts and a mask
             writeBit(bit);                          // Write each bit to the file, using writeBit()
         }
@@ -69,6 +74,6 @@ void BitStream_Write::writeString(const std::string &str) {
     // Iterate through each string character and write it to the file using writeNBits.
     for(int i=0; i<str.length(); i++) {
         writeNBits(str[i], 8);  // Using operator[] to access the character instead of at(). It is faster, however
-                                     // it does not check if 'i' is a valid position or not. Must be used with caution.
+        // it does not check if 'i' is a valid position or not. Must be used with caution.
     }
 }
